04eba03045ee1745da966550a6123b7149e0d0f1
Valentyn Rubliuk <vrubliuk@gmail.com>
2019-09-28 16:34:41 +0300

    Updates

---
+++ precache-manifest.9018c6038d808e29ee3c10366ca2b8bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6f7d9e318d0fa06c5cd800a46d6dc0",
+    "url": "/font-preview/index.html"
+  },
+  {
+    "revision": "4e492f344797010d793a",
+    "url": "/font-preview/static/css/main.6bc9c99a.chunk.css"
+  },
+  {
+    "revision": "fb51a177c3b635dfe399",
+    "url": "/font-preview/static/js/2.f1001e95.chunk.js"
+  },
+  {
+    "revision": "4e492f344797010d793a",
+    "url": "/font-preview/static/js/main.bcffb9e3.chunk.js"
+  },
+  {
+    "revision": "e4e4d1a3fc8b22f7fcb9",
+    "url": "/font-preview/static/js/runtime~main.405e5ed9.js"
+  }
+]);
